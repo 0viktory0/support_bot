91dceabe5b502908e9369bc721d9b19d60d2fce7
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-06-18 22:54:49 +0300

    add DialogFlow learning script

---
+++ telegram_bot.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+from dialog_flow import detect_intent_texts
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Привет {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Сам ты Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    response = detect_intent_texts(project_id, update.effective_chat.id, [update.message.text], "ru")
+    update.message.reply_text(response.fulfillment_text)
+
+
+def main() -> None:
+    updater = Updater(tg_token)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    project_id = os.getenv("DIALOGFLOW_PROJECT_ID")
+    tg_token = os.getenv("TG_BOT_TOKEN")
+
+    main()
